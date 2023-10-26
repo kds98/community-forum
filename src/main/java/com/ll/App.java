61e43bf395313b4b0f6abd831b2921005ff4b625
kds98 <skyudae@naver.com>
2023-10-26 17:44:50 +0900

    level 7

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -12,8 +12,8 @@ public class App {
 
     void run() {
         System.out.println("== 명언 앱 ==");
-        while (true) {
 
+        while (true) {
             System.out.print("명령) ");
             command = scanner.nextLine();
             if (command.equals("종료")) {
@@ -22,15 +22,16 @@ public class App {
                 create();
             } else if (command.equals("목록")) {
                 listView();
-            } else if (command.equals("삭제")) {
+            } else if (command.startsWith("삭제?")) {
                 delete();
+            } else if (command.startsWith("수정?")) {
+                modify();
             }
         }
         System.exit(0);
     }
 
     void create() {
-
         System.out.print("명언 : ");
         String content = scanner.nextLine();
         System.out.print("작가 : ");
@@ -54,6 +55,55 @@ public class App {
     }
 
     void delete() {
+        int id = getParamAsInt("id", 0);
 
+        if (id == 0) {
+            System.out.println("id를 정확히 입력해주세요.");
+            return; // 함수 종료
+        }
+        try {
+            quotes.remove(id - 1);
+            System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
+        } catch (Exception e) {
+            System.out.printf("%d번 명언이 존재하지 않습니다.\n", id);
+            return;
+        }
+    }
+
+    void modify() {
+        int id = getParamAsInt("id", 0);
+
+        if (id == 0) {
+            System.out.println("id를 정확히 입력해주세요.");
+            return; // 함수 종료
+        }
+        System.out.printf("%d번 명언을 수정합니다.\n", id);
+    }
+
+    int getParamAsInt(String paramName, int defaultValue) {
+        String[] commandBits = command.split("\\?", 2);
+        String queryString = commandBits[1];
+
+        String[] queryStringBits = queryString.split("&");
+
+        for (int i = 0; i < queryStringBits.length; i++) {
+            String queryParamStr = queryStringBits[i];
+
+            String[] queryParamStrBits = queryParamStr.split("=", 2);
+
+            String _paramName = queryParamStrBits[0];
+            String paramValue = queryParamStrBits[1];
+
+            if (_paramName.equals(paramName)) {
+                try {
+                    // 문제 없는 경우
+                    return Integer.parseInt(paramValue);
+                } catch (NumberFormatException e) {
+                    // 문제가 생긴 경우
+                    return defaultValue;
+                }
+            }
+        }
+        return defaultValue;
     }
 }
